3002c1d2ff898ef17b0f3a38db3e5758374e595b
amaralkaff <tpamangly@gmail.com>
2023-11-21 15:02:32 +0700

    feat: add diagonal

--- game/index.js
+++ game/index.js
@@ -24,130 +24,141 @@
  * you can't make a move anymore, otherwise it will throw an error.
  */
 class Gomoku {
-    #turn;
-    #SIZE;
-    #isFinished;
-    #board;
-    constructor(size = 15) {
-        this.#SIZE = size;
-        this.#isFinished = false;
-        this.#turn = 1;
-        this.#board = this.#initBoard();
-    }
+  #turn;
+  #SIZE;
+  #isFinished;
+  #board;
+  constructor(size = 15) {
+    this.#SIZE = size;
+    this.#isFinished = false;
+    this.#turn = 1;
+    this.#board = this.#initBoard();
+  }
 
-    get status() {
-        return {
-            board: this.#board,
-            turn: this.#turn,
-            lastPlayer: this.#getLastPlayer(),
-            nextPlayer: this.#getNextPlayer(),
-            status: this.#isFinished ? "Finished" : "Ongoing",
-        };
-    }
+  get status() {
+    return {
+      board: this.#board,
+      turn: this.#turn,
+      lastPlayer: this.#getLastPlayer(),
+      nextPlayer: this.#getNextPlayer(),
+      status: this.#isFinished ? "Finished" : "Ongoing",
+    };
+  }
 
-    #getLastPlayer() {
-        if (this.#turn == 1) return;
-        if (this.#turn % 2 == 0) {
-            return "X";
-        } else {
-            return "O";
+  #getLastPlayer() {
+    if (this.#turn == 1) return;
+    if (this.#turn % 2 == 0) {
+      return "X";
+    } else {
+      return "O";
+    }
+  }
+
+  #getNextPlayer() {
+    if (this.#turn % 2 == 0) {
+      return "O";
+    } else {
+      return "X";
+    }
+  }
+
+  #initBoard() {
+    let board = [];
+    for (let i = 0; i < this.#SIZE; i++) {
+      board.push([]);
+      for (let j = 0; j < this.#SIZE; j++) {
+        board[i].push(null);
+      }
+    }
+    return board;
+  }
+
+  /**
+   * Checks for winning condition, and returns true
+   * (if a win is found) or false (if a win is not found).
+   */
+  #checkWin(x, y) {
+    // vertical
+    for (let i = 0; i < this.#SIZE - 4; i++) {
+      if (
+        this.#board[i][y] &&
+        this.#board[i + 1][y] === this.#board[i][y] &&
+        this.#board[i + 2][y] === this.#board[i][y] &&
+        this.#board[i + 3][y] === this.#board[i][y] &&
+        this.#board[i + 4][y] === this.#board[i][y]
+      ) {
+        return true;
+      }
+    }
+    // horizontal
+    for (let i = 0; i < this.#SIZE - 4; i++) {
+      if (
+        this.#board[x][i] &&
+        this.#board[x][i + 1] === this.#board[x][i] &&
+        this.#board[x][i + 2] === this.#board[x][i] &&
+        this.#board[x][i + 3] === this.#board[x][i] &&
+        this.#board[x][i + 4] === this.#board[x][i]
+      ) {
+        return true;
+      }
+    }
+    // diagonal
+    // TODO: ...
+    for (let i = 0; i < this.#SIZE - 4; i++) {
+      if (
+        this.#board[i][i] &&
+        this.#board[i + 1][i + 1] === this.#board[i][i] &&
+        this.#board[i + 2][i + 2] === this.#board[i][i] &&
+        this.#board[i + 3][i + 3] === this.#board[i][i] &&
+        this.#board[i + 4][i + 4] === this.#board[i][i]
+      ) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /* returns true if the next move results in a win, false otherwise */
+  move(x, y) {
+    if (this.#isFinished) throw new Error("Game is finished");
+
+    for (let i = 0; i < this.#SIZE; i++) {
+      for (let j = 0; j < this.#SIZE; j++) {
+        if (i === x && j === y) {
+          // TODO: move validation
+          // ...
+
+          // if move is valid, do the following
+          this.#board[i][j] = this.#getNextPlayer();
+          if (this.#checkWin(x, y)) {
+            this.#isFinished = true;
+            return true;
+          }
+          this.#turn++;
         }
+      }
     }
+    return false;
+  }
 
-    #getNextPlayer() {
-        if (this.#turn % 2 == 0) {
-            return "O";
-        } else {
-            return "X";
-        }
-    }
-
-    #initBoard() {
-        let board = [];
-        for (let i = 0; i < this.#SIZE; i++) {
-            board.push([]);
-            for (let j = 0; j < this.#SIZE; j++) {
-                board[i].push(null);
-            }
-        }
-        return board;
-    }
-
-    /**
-     * Checks for winning condition, and returns true
-     * (if a win is found) or false (if a win is not found).
-     */
-    #checkWin(x, y) {
-        // vertical
-        for (let i = 0; i < this.#SIZE - 4; i++) {
-            if (
-                this.#board[i][y] &&
-                this.#board[i + 1][y] === this.#board[i][y] &&
-                this.#board[i + 2][y] === this.#board[i][y] &&
-                this.#board[i + 3][y] === this.#board[i][y] &&
-                this.#board[i + 4][y] === this.#board[i][y]
-            ) {
-                return true;
-            }
-        }
-        // horizontal
-        for (let i = 0; i < this.#SIZE - 4; i++) {
-            if (
-                this.#board[x][i] &&
-                this.#board[x][i + 1] === this.#board[x][i] &&
-                this.#board[x][i + 2] === this.#board[x][i] &&
-                this.#board[x][i + 3] === this.#board[x][i] &&
-                this.#board[x][i + 4] === this.#board[x][i]
-            ) {
-                return true;
-            }
-        }
-        // diagonal
-        // TODO: ...
-        return false;
-    }
-
-    /* returns true if the next move results in a win, false otherwise */
-    move(x, y) {
-        if (this.#isFinished) throw new Error("Game is finished");
-
-        for (let i = 0; i < this.#SIZE; i++) {
-            for (let j = 0; j < this.#SIZE; j++) {
-                if (i === x && j === y) {
-                    // TODO: move validation
-                    // ...
-
-                    // if move is valid, do the following
-                    this.#board[i][j] = this.#getNextPlayer();
-                    if (this.#checkWin(x, y)) {
-                        this.#isFinished = true;
-                        return true;
-                    }
-                    this.#turn++;
-                }
-            }
-        }
-        return false;
-    }
-
-    /* reset back to the beginning of the game */
-    reset() {
-        this.#initBoard();
-        this.#turn = 1;
-        this.#isFinished = false;
-    }
+  /* reset back to the beginning of the game */
+  reset() {
+    this.#initBoard();
+    this.#turn = 1;
+    this.#isFinished = false;
+  }
 }
 
-// const game = new Gomoku();
-// game.move(0, 1);
-// game.move(8, 8);
-// game.move(1, 1);
-// game.move(8, 8);
-// game.move(2, 1);
-// game.move(8, 8);
-// game.move(3, 1);
-// game.move(8, 8);
-// game.move(4, 1);
-// console.log(game.status);
+const game = new Gomoku();
+game.move(0, 1);
+game.move(8, 8);
+game.move(1, 1);
+game.move(8, 8);
+game.move(2, 1);
+game.move(8, 8);
+game.move(3, 1);
+game.move(8, 8);
+game.move(4, 1);
+console.log(game.status);
 
 module.exports = Gomoku;
