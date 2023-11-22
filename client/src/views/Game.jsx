7f98235c679a19434cfeef708b4ddf5fa54b790e
aeross <andrewpratama22@gmail.com>
2023-11-22 21:41:31 +1100

    reset game status at the beginning of the game

--- client/src/views/Game.jsx
+++ client/src/views/Game.jsx
@@ -5,14 +5,40 @@ import Board from "../components/Board";
 import { useDispatch, useSelector } from "react-redux";
 import { reset } from "../slices/gomokuSlice";
 
+// firebase
+import { database } from "../firebase/firebaseConfig";
+import { ref, update } from "firebase/database";
+
 export default function Game() {
-  const isFinished = useSelector((state) => state.gomoku.isFinished);
-  const turn = useSelector((state) => state.gomoku.turn);
+  // const isFinished = useSelector((state) => state.gomoku.isFinished);
+  // const turn = useSelector((state) => state.gomoku.turn);
+  const { turn, isFinished, board, lastMoveByX, lastMoveByO, disabled } = useSelector((state) => state.gomoku);
+
   const dispatch = useDispatch();
 
+  // reset board back to the beginning before starting the game
   useEffect(() => {
-    dispatch(reset());
+    const player = localStorage.getItem("user_id");
+    if (player == "p1") dispatch(reset());
   }, []);
+
+  // check if connected
+  function setStatusConnected(player) {
+    const updates = {};
+    updates[`gomokuGame/${player}/join`] = true;
+    update(ref(database), updates);
+  }
+
+  useEffect(() => {
+    const player = localStorage.getItem("user_id");
+    if (player == "p1") {
+      // connected as player 1
+      setStatusConnected("p1");
+    } else if (player == "p2") {
+      // connected as player 2
+      setStatusConnected("p2");
+    }
+  })
 
   return (
     <>
